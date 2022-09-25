196156e1fa333112bfa898149eb8d34d27408b0b
Rafael Farias <rafaelfarias@zohomail.com>
2022-09-25 14:56:48 -0300

    feat(installer): add two lines after each apt install

--- installer.sh
+++ installer.sh
@@ -6,6 +6,7 @@ function install {
     if [ $? -ne 0 ]; then
         echo "Installing: ${1}..."
         sudo apt install -y $1
+        echo "\n\n"
     else
         echo "Already installed: ${1}"
     fi
